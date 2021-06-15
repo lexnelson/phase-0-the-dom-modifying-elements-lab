21fac7fba54b45255716803e9786f2d6a2e9357e
Lex Nelson <lexinelson597@gmail.com>
2021-06-15 10:41:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const el = document.getElementById('main');
+el.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+victory.innerHTML = "<h1> Lex is the champion</h1>";
